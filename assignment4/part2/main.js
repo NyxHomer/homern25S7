3faf37e798011ccd983fd60e0c4c025d8915d527
NyxHomer <nyxhomer@gmail.com>
2025-07-23 15:24:37 -0400

    Made array of images

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -12,6 +12,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [
+    "./images/pic1.jpg",
+    "./images/pic2.jpg",
+    "./images/pic3.jpg",
+    "./images/pic4.jpg",
+    "./images/pic5.jpg"
+]
 
 /* Declaring the alternative text for each image file */
 
