4f13c871354c58d8e3d6f2a3250c4c63a857e39f
NyxHomer <nyxhomer@gmail.com>
2025-07-27 17:10:25 -0400

    Made shapes class

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -25,12 +25,19 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+// Create Shape class
+class Shape{
+  constructor(x, y, velX, velY, color, size){
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class Ball extends Shape{
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
   }
