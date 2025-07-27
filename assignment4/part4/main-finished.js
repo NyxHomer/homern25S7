0ef12706df6933c0e82c4e23beb5d238600f07ed
NyxHomer <nyxhomer@gmail.com>
2025-07-27 17:15:38 -0400

    Made evil circle

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -34,12 +34,13 @@ class Shape{
     this.velY = velY;
   }
 }
-
+// Create ball class
 class Ball extends Shape{
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -72,7 +73,7 @@ class Ball extends Shape{
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -82,6 +83,39 @@ class Ball extends Shape{
         }
       }
     }
+  }
+}
+
+// Create EvilCircle class
+class EvilCircle extends Shape{
+  constructor(x, y){
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
+  }
+  // Draw method
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
 
